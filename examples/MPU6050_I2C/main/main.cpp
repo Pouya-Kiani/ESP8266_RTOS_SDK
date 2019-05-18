242a94b110a1a7600f0c970dea83a6ddc158a992
huhu <49025826+Pouya-Kiani@users.noreply.github.com>
2019-05-19 00:55:15 +0430

    Create main.cpp

---
+++ examples/MPU6050_I2C/main/main.cpp
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <inttypes.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+
+#include "user_src/MPU_6050.cpp"
+
+static const char *TAG = "app_main";
+
+/**
+ * Brief:
+ * This test code shows how to configure gpio and how to use gpio interrupt.
+ *
+ * GPIO status:
+ * GPIO15: output
+ * GPIO16: output
+ * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
+ * GPIO5:  input, pulled up, interrupt from rising edge.
+ *
+ * Test:
+ * Connect GPIO15 with GPIO4
+ * Connect GPIO16 with GPIO5
+ * Generate pulses on GPIO15/16, that triggers interrupt on GPIO4/5
+ *
+ */
+
+#define GPIO_OUTPUT_IO_0    GPIO_NUM_14
+#define GPIO_OUTPUT_IO_1    GPIO_NUM_16
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+#define GPIO_INPUT_IO_0     GPIO_NUM_12
+#define GPIO_INPUT_IO_1     GPIO_NUM_13
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+extern "C"{
+    void app_main(void);
+}
+//#include "Stuff_Handler.c"
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void gpio_isr_handler(void *arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void *arg)
+{
+    gpio_num_t io_num;
+
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+class counter
+{
+    public:
+    int cnt;
+    counter()
+    {
+        cnt = 0;
+    }
+        
+
+    int increase()
+    {
+        return cnt++;
+    }
+};
+
+// Task functions prototypes, bodies are defined after app_main
+static void MPU6050_task_example(void *arg);
+
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "initializing Timer");
+    esp_err_t esp_timer_init();
+
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO15/16
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    //disable pull-up mode
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&io_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *) GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
+
+    counter c;
+    xTaskCreate(MPU6050_task_example, "i2c_task_example", 2048, NULL, 10, NULL);
+    
+}
+static void MPU6050_task_example(void *arg)
+{
+    static const char *TAG = "task_read_data";
+    MPU6050 my_IMU;
+    my_IMU.initMPU6050();
+    int16_t tempData[3];
+
+    while ((1))
+    {
+        my_IMU.readAccelData(tempData);
+        ESP_LOGI(TAG, "accRawData: %d %d %d \n", tempData[0], tempData[1], tempData[2]);
+        my_IMU.readGyroData(tempData);
+        ESP_LOGI(TAG, "GyroRawData: %d %d %d \n", tempData[0], tempData[1], tempData[2]);
+
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+    
+    
+}
+static void Interrupt_task_example(void *arg)
+{
+    gpio_num_t io_num;
+
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
